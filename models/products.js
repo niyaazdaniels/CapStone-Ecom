bf042abb8085d8d274f14c5195ce544048fdf97f
Niyaaz Daniels <niyaazdaniels1@gmail.com>
2024-03-15 14:37:10 +0200

    Corrected Edit

--- models/products.js
+++ models/products.js
@@ -24,27 +24,27 @@ const getExistingProduct = async (prodID) => {
     return result;
 };
 
-const addNewProduct = async (prodName, prodDesc, category, price, quantity, prodImage) => {
+const addNewProduct = async (prodName, prodDesc,quantity, price, category, prodImage) => {
 
     const [product] = await pool.query(`
 
-        INSERT INTO Products (prodName, prodDesc, category, price, quantity, prodImage) 
+        INSERT INTO Products (prodName, prodDesc, quantity, price, category, prodImage) 
         VALUES (?, ?, ?, ?, ?, ?)`,
 
-        [prodName, prodDesc, category, price, quantity, prodImage]);
+        [prodName, prodDesc ,quantity, price, category, prodImage ]);
 
     return getExistingProduct(product.insertId);
 };
 
-const editExistingProduct = async (prodID, prodName, prodDesc, category, price, quantity, prodImage) => {
+const editExistingProduct = async (prodID,prodName, prodDesc, quantity, price, category, prodImage) => {
 
     await pool.query(`
 
         UPDATE Products
-        SET prodName = ?, prodDesc = ? ,category = ?, price = ? , quantity = ?, prodImage = ?
+        SET prodName = ?, prodDesc = ? , quantity = ?, price = ? , category = ?, prodImage = ?
         WHERE prodID = ?`,
 
-        [prodName, prodDesc, category, price, quantity, prodImage, prodID]);
+        [(prodName, prodDesc, quantity, price,category, prodImage) ]);
 
     const editedProduct = await getExistingProduct(prodID);
 
